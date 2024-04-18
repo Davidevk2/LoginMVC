e0e5316e31fcb19e992767f4077fd3c16dd43c97
Davidevk2 <k2guffy@gmail.com>
2024-04-17 20:23:48 -0500

    Listar historial del empleado

--- Controllers/HistorialController.cs
+++ Controllers/HistorialController.cs
@@ -21,24 +21,28 @@ namespace SistemaLaboral.Controllers
 
         public async Task<IActionResult> Index()
         {
-            ViewBag.IdEmleado = HttpContext.Session.GetString("IdEmpleado"); //variable de sesion para la vista
+            int id  = Int32.Parse(HttpContext.Session.GetString("IdEmpleado")); //variable de sesion para la vista
             ViewBag.Nombre = HttpContext.Session.GetString("Nombre");
-            return View( await _context.Historial.ToListAsync());
-        }
 
+            //Listar solo los registros del empleado
+            var result = from historial in   _context.Historial select historial;
+            result = result.Where(his => his.IdEmpleado == id);
+
+            return View( await result.ToListAsync());
+             
+        }
+        
         [HttpPost]
         public async Task<IActionResult> RegistrarIngreso( [Bind("IdEmpleado")]Historial historial){
-            var id = HttpContext.Session.GetString("IdEmpleado");
-        return Json(id);
+            var id = Int32.Parse(HttpContext.Session.GetString("IdEmpleado"));
+        
             try{
+                historial.IdEmpleado =id;
+
+                _context.Historial.Add(historial);
+                await _context.SaveChangesAsync();
                 
-
-                    historial.IdEmpleado =Convert.ToInt32(id);
-
-                    _context.Historial.Add(historial);
-                    await _context.SaveChangesAsync();
-                    
-                    return RedirectToAction("Index", "Empleados");
+                return RedirectToAction("Index", "Empleados");
 
 
             }catch(Exception ex){
@@ -50,7 +54,7 @@ namespace SistemaLaboral.Controllers
         }
 
         
-
+ 
        
     }
 }
