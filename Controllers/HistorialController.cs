37f713c0668f95269d733a8f20d5a16c999e9485
Davidevk2 <k2guffy@gmail.com>
2024-04-19 21:24:35 -0500

    Editar observacion

--- Controllers/HistorialController.cs
+++ Controllers/HistorialController.cs
@@ -7,37 +7,40 @@ using SistemaLaboral.Models;
 
 namespace SistemaLaboral.Controllers
 {
-    
+
     public class HistorialController : Controller
     {
         //Inyeccion de dependencias para la conexion a la db
-        public readonly SistemaContext _context; 
-        
+        public readonly SistemaContext _context;
+
         //Constructor que inicializa la conexion 
         public HistorialController(SistemaContext context)
         {
             _context = context;
         }
 
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(int pagina = 1)
         {
-            int id  = Int32.Parse(HttpContext.Session.GetString("IdEmpleado")); //variable de sesion para la vista
+            
+            int id = Int32.Parse(HttpContext.Session.GetString("IdEmpleado")); //variable de sesion para la vista
             ViewBag.Nombre = HttpContext.Session.GetString("Nombre");
             //Listar solo los registros del empleado
-            var result = from historial in   _context.Historial select historial;
-
+            var result = from historial in _context.Historial select historial;
             result = result.Where(his => his.IdEmpleado == id);
 
-            return View( await result.ToListAsync());
-             
+            return View(await result.ToListAsync());
+
         }
-        
-        public async Task<IActionResult> RegistrarIngreso(){
+
+        public async Task<IActionResult> RegistrarIngreso()
+        {
             var id = Int32.Parse(HttpContext.Session.GetString("IdEmpleado"));
-        
-            try{
+
+            try
+            {
                 //objeto de registro
-                var historial = new Historial(){
+                var historial = new Historial()
+                {
                     IdEmpleado = id,
                     FechaIngreso = DateTime.Now
 
@@ -45,44 +48,56 @@ namespace SistemaLaboral.Controllers
 
                 _context.Historial.Add(historial);
                 await _context.SaveChangesAsync();
-                
+
                 TempData["MessageSuccess"] = "Se ha registrado con exito!";
                 return RedirectToAction("Index", "Empleados");
 
 
-            }catch(Exception ex){
-                ViewBag.ErrorMessage = "Error"+ ex;
+            }
+            catch (Exception ex)
+            {
+                ViewBag.ErrorMessage = "Error" + ex;
                 return View(ViewBag.ErrorMessage);
             }
-           
+
 
         }
 
-         public async Task<IActionResult> RegistrarSalida(){
+        public async Task<IActionResult> RegistrarSalida()
+        {
             var id = Int32.Parse(HttpContext.Session.GetString("IdEmpleado"));
-        
-            try{
+
+            try
+            {
                 var historial = await _context.Historial.OrderBy(his => his.Id).LastOrDefaultAsync(his => his.IdEmpleado == id && his.FechaSalida == null);
 
                 historial.FechaSalida = DateTime.Now;
                 await _context.SaveChangesAsync();
-                
+
                 TempData["MessageSuccess"] = "Se ha registrado con exito!";
                 return RedirectToAction("Index", "Empleados");
 
 
-            }catch(Exception ex){
-                ViewBag.ErrorMessage = "Error"+ ex;
+            }
+            catch (Exception ex)
+            {
+                ViewBag.ErrorMessage = "Error" + ex;
                 return View(ViewBag.ErrorMessage);
             }
-           
+
 
         }
 
-        
+        //Vista de editar observacion 
+        public async Task<IActionResult> Editar(int id)
+        {
+            return View(await _context.Historial.FirstOrDefaultAsync(h => h.Id == id));
+        }
 
-        
- 
-       
+
+
+
+
+
     }
 }
